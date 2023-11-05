79ea1b526f2fc4e1d70c3eccfe379191c2a69328
chentedataengineer <chentedataengineer>
2023-11-05 17:35:22 +0000

    Workspace migration commit
    Github repo alignment to migrate workspace as S3 is not working

--- code/global/ETL/BoilerPlate/mainBoiler.py
+++ code/global/ETL/BoilerPlate/mainBoiler.py
@@ -1,15 +1,17 @@
 # Databricks notebook source
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum
+from pyspark.sql import *
 
 # COMMAND ----------
 
 #TODO: Technology and file format to be created as input parameters
 def fileReader(payload, spark):
     # Leer dataframe
-    df = spark.read.option("header",payload["file_header"]).format(payload["file_format"]).load(payload["file_input_path"]+"/"+payload["file_name"]+"."+payload["file_format"])
-    return df
+    if (payload["file_format"] == "csv"):
+        df = spark.read.option("header",payload["file_header"]).format(payload["file_format"]).load(payload["file_input_path"]+"/"+payload["file_name"]+"."+payload["file_format"])
+        return df
+    elif (payload["file_format"] == "json"):
+        df = spark.read.format(payload["file_format"]).load(payload["file_input_path"]+"/"+payload["file_name"]+"."+payload["file_format"])
+        return df
 
 def dataframeToDeltaTable(payload, df, spark_session, writing_mode):
     # Guarda el DataFrame como tabla Delta en el path de salida
@@ -19,3 +21,28 @@ def dataframeToDeltaTable(payload, df, spark_session, writing_mode):
     except Exception as e:
         print(e)
 
+
+# COMMAND ----------
+
+#TODO: deduplication function
+def dataframeDeduplication(df, primary_key_fields):
+    for field in primary_key_fields:
+        if field not in df.columns:
+            raise ValueError(f"El campo '{field}' no está presente en el DataFrame.")
+
+    deduplicated_df = df.dropDuplicates(subset=primary_key_fields)
+
+    return deduplicated_df
+#TODO: schema parsing function
+def dataframeParsing(df, type_dict):
+    """
+    Convierte las columnas del DataFrame a los tipos de datos especificados en el diccionario type_dict.
+
+    :param df: DataFrame de PySpark.
+    :param type_dict: Diccionario que mapea nombres de columnas a tipos de datos deseados.
+    :return: DataFrame con las columnas convertidas a los tipos de datos especificados.
+    """
+    for col_name, col_type in type_dict.items():
+        if col_name in df.columns:
+            df = df.withColumn(col_name, col(col_name).cast(col_type))
+    return df
